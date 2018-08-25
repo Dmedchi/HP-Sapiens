24357da1b86bb82e7d51d462de87ae80b84637b5
Dmedchi <33534842+Dmedchi@users.noreply.github.com>
2018-08-26 01:06:37 +0300

    Постоянные переменные в отдельном скрипте

---
+++ constants.py
@@ -0,0 +1,4 @@
+RETURN_URL = 'http://localhost/auth/linkedin'
+URL_FOR_OAUTH = 'https://www.linkedin.com/oauth/v2/authorization?'
+URL_FOR_TOKEN = 'https://www.linkedin.com/oauth/v2/accessToken'
+STATE = '987654321'
